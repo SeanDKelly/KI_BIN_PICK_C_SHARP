bf794b90a856f93c452031f9fc6c4eca51139cd9
SeanDKelly <stsekell@hochschulekempten.onmicrosoft.com>
2023-06-26 17:21:14 +0200

    Solidworks Klasse erstellt

--- KI_BIN_PICK_C#/Program.cs
+++ KI_BIN_PICK_C#/Program.cs
@@ -4,58 +4,94 @@ using System.Reflection;
 using SolidWorks.Interop.sldworks;
 using SolidWorks.Interop.swconst;
 
-
-
-// SolidWorks-Anwendung erstellen
-SldWorks swApp = null;
-ModelDoc2 swModel = null;
-MassProperty2 MyMassProp = null;
-ModelDocExtension Extn = null;
-int longstatus = 0;
-int longwarnings = 0;
-double[] pmoi;
-double[] vValue;
-double[] value = new double[3];
-int errors;
-int warnings;
-double val;
-
-try
+public class SW
 {
-    swApp = Activator.CreateInstance(Type.GetTypeFromProgID("SldWorks.Application")) as SldWorks;
-    swApp.Visible = true; // SolidWorks sichtbar machen
+
+    public SldWorks swApp = null;
+
+
+    // Constructor
+    public SW()
+    {
+        // Erstellt eine SolidWorks instanz bzw öffnet SolidWorks
+
+        swApp = Activator.CreateInstance(Type.GetTypeFromProgID("SldWorks.Application")) as SldWorks;
+        swApp.Visible = true; // SolidWorks sichtbar machen
+    }
+
+    // Destructor
+    ~SW()
+    {
+        // Ressourcen freigeben bzw SowlidWorks schließen
+        /*
+        if (swModel != null)
+        {
+            swModel.Close();
+            swModel = null;
+        }
+        */
+        if (swApp != null)
+        {
+            swApp.ExitApp();
+            swApp = null;
+        }
+       
+        Console.WriteLine("Drücken Sie eine beliebige Taste, um das Programm zu beenden.");
+        Console.ReadKey();
+    }
 
     // Bauteil öffnen
-    //(int)swOpenDocOptions_e.swOpenDocOptions_ViewOnly,
-
-    swModel = swApp.OpenDoc6("C:\\Users\\theja\\Downloads\\Greifer.SLDPRT",
-                                            (int)swDocumentTypes_e.swDocPART,
-                                            (int)swOpenDocOptions_e.swOpenDocOptions_Silent,
-                                            "",
-                                            ref longstatus,
-                                            ref longwarnings);
-                
-
-
-
-    if (swModel != null)
+    public ModelDoc2 OpenPart(string directory)
     {
-        // Bauteil erfolgreich geöffnet
-        Console.WriteLine("Bauteil erfolgreich geöffnet: " + swModel.GetTitle());
+        int longstatus = 0;
+        int longwarnings = 0;
+        ModelDoc2 swModel = null;
 
-        // Schwerpunkt berechnen
-        Extn = swModel.Extension;
+        // "C:\\Users\\theja\\Downloads\\Greifer.SLDPRT"
+        swModel = swApp.OpenDoc6(directory,
+                                (int)swDocumentTypes_e.swDocPART,
+                                (int)swOpenDocOptions_e.swOpenDocOptions_Silent,
+                                "",
+                                ref longstatus,
+                                ref longwarnings);
+        if (swModel != null)
+        {
+            // Bauteil erfolgreich geöffnet
+            Console.WriteLine("Bauteil erfolgreich geöffnet: " + swModel.GetTitle());
+        }
+
+        else
+        {
+            Console.WriteLine("Fehler beim Öffnen des Bauteils!");
+        }
+
+        return swModel;
+    }
+
+    // Schwerpunkt berechnen
+    public double[] CenterOfMassPart(ModelDoc2 Part)
+    {
+        MassProperty2 MyMassProp = null;
+        ModelDocExtension Extn = null;
+        double[] pmoi;
+        double[] vValue;
+        double[] value = new double[3];
+        double val;
 
 
+        // Extended Properties of Part
+        Extn = Part.Extension;
 
         // Create mass properties and override options
-
         MyMassProp = (MassProperty2)Extn.CreateMassProperty2();
 
         // Use document property units (MKS)
-
         MyMassProp.UseSystemUnits = false;
 
+        // Schwerpunkt
+        value = (double[])MyMassProp.CenterOfMass;
+
+        /*
         Console.WriteLine("Mass properties before override");
         Console.WriteLine("");
         val = MyMassProp.Mass;
@@ -72,31 +108,23 @@ try
         Console.WriteLine("Principal moments of inertiae: Px: " + pmoi[0] + ", Py: " + pmoi[1] + ", and Pz: " + pmoi[2]);
         vValue = (double[])MyMassProp.GetMomentOfInertia(0);
         Console.WriteLine("Moments of inertia: Lxx: " + vValue[0] + ", Lxy: " + vValue[1] + ", Lxz: " + vValue[2] + ", Lyx: " + vValue[3] + ", Lyy: " + vValue[4] + ", Lyz: " + vValue[5] + ", Lzx: " + vValue[6] + ", Lzy: " + vValue[7] + ", Lzz: " + vValue[8]);
+        */
+
+        return value;
     }
-    else
-    {
-        Console.WriteLine("Fehler beim Öffnen des Bauteils!");
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine("Fehler: " + ex.Message);
-}
-finally
-{
-    // Ressourcen freigeben
-    if (swModel != null)
-    {
-        swModel.Close();
-        swModel = null;
-    }
-    if (swApp != null)
-    {
-        swApp.ExitApp();
-        swApp = null;
-    }
+
 }
 
-Console.WriteLine("Drücken Sie eine beliebige Taste, um das Programm zu beenden.");
-Console.ReadKey();
-
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Starte SolidWorks");
+        SW SWProgramm = new SW();
+        Console.WriteLine("Bauteil Öffnen");
+        ModelDoc2 Greifer = SWProgramm.OpenPart("C:\\Users\\theja\\Downloads\\Greifer.SLDPRT");
+        Console.WriteLine("Schwerpunkt berechnen");
+        double[] SchwerpunktVektor = SWProgramm.CenterOfMassPart(Greifer);
+        Console.WriteLine("Schwerpunkt: X:" + SchwerpunktVektor[0] + " Y:" + SchwerpunktVektor[1] + " Z:" + SchwerpunktVektor[2]);
+    }
+}
